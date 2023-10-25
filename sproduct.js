8d80b0c7fa2e3ee8d0be3a3c805fe72d93265a92
yadavkapil99 <yadavkapil0706@gmail.com>
2023-10-25 21:48:17 +0530

    commit on child branch

--- sproduct.js
+++ sproduct.js
@@ -1,34 +1,38 @@
-
 var productImage = document.querySelectorAll('.i');
 
 var mainImg = document.getElementById('main-image');
 var smallImg = document.querySelectorAll('.small-img');
 
-productImage.forEach((e) => {
-  e.addEventListener('click', (el) => {
-    // var i = el.src;
-    window.location.href = 'sproduct.html';
-    console.log("hello");
-    // mainImg.src = i; 
-  });
-});
-
-
-
-// var images = [];
-
-// function openSProduct() {
-//   productImage.forEach((e) => {
-//     e.addEventListener('click', (el) => {
-//       var i = el.src;
-//       images.push(i);
-//       window.location.href = 'sproduct.html';
-//       mainImg.src = i;
-//     });
-//     console.log(images.length);
+// productImage.forEach((e) => {
+//   e.addEventListener('click', (el) => {
+//     mainImg = el.target.src;
+//     console.log(el.target)
+//     console.log('main ===',mainImg)
+   
+//     // window.location.href = 'sproduct.html';
 //   });
-// }
-// openSProduct();
+// });
+
+
+
+
+var images = [];
+
+productImage.forEach((e) => {
+    e.addEventListener('click', (el) => {
+      var i = el.target.src;
+      // console.log(i)
+
+      images.push(i);
+      console.log(images);
+      window.location.href = 'sproduct.html';
+      mainImg.src = images[images.length-1];
+      console.log(mainImg.src);
+    });
+  });
+
+
+  
 
 
 for(let i=0;i<4;i++){
